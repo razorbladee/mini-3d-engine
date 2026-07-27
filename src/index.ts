3fb39429768c7a2751e1edca84264b2fefbc303c
razorbladee <104161605+razorbladee@users.noreply.github.com>
2026-07-27 15:16:02 +0300

    feat: expose initial public API

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export * from './math/Vector3';
+export * from './math/Matrix4';
+export * from './core/Node';
+export * from './core/Scene';
+export * from './cameras/Camera';
+export * from './cameras/PerspectiveCamera';
