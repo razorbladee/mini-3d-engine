566a30425e1275d2305579fef9aab8420b8304de
razorbladee <104161605+razorbladee@users.noreply.github.com>
2026-07-27 15:32:54 +0300

    feat: add active camera slot to Scene

--- src/core/Scene.ts
+++ src/core/Scene.ts
@@ -1,1 +1,1 @@
-import {Node} from './Node'; export class Scene extends Node {background='#101522'}
+import {Node} from './Node'; import type {Camera} from '../cameras/Camera'; export class Scene extends Node { background='#101522'; activeCamera:Camera|null=null; }
