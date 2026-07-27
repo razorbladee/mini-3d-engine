ecb1e58f05f52b3d6358e19ab0d8209d67931a71
razorbladee <104161605+razorbladee@users.noreply.github.com>
2026-07-27 15:27:20 +0300

    feat: add PlaneGeometry

---
+++ src/geometry/PlaneGeometry.ts
@@ -0,0 +1,1 @@
+import {BufferGeometry} from './BufferGeometry'; export class PlaneGeometry extends BufferGeometry { constructor(width=1,height=1){const x=width/2,y=height/2;super([-x,-y,0,x,-y,0,x,y,0,-x,-y,0,x,y,0,-x,y,0])} }
