45834bf801ff9dc091bf823e42207bed88380d93
razorbladee <104161605+razorbladee@users.noreply.github.com>
2026-07-27 16:28:25 +0300

    feat: support UVs and diffuse texture maps

--- src/materials/StandardMaterial.ts
+++ src/materials/StandardMaterial.ts
@@ -1,1 +1,8 @@
-import {BasicMaterial} from './BasicMaterial'; export class StandardMaterial extends BasicMaterial { constructor(options:{color?:string;roughness?:number;metalness?:number}={}){super(options);this.roughness=options.roughness??0.5;this.metalness=options.metalness??0} roughness:number; metalness:number }
+import { BasicMaterial } from './BasicMaterial';
+import { Texture2D } from '../rendering/Texture2D';
+
+export class StandardMaterial extends BasicMaterial {
+  roughness: number;
+  metalness: number;
+  constructor(options: { color?: string; opacity?: number; transparent?: boolean; wireframe?: boolean; doubleSided?: boolean; roughness?: number; metalness?: number; map?: Texture2D } = {}) { super(options); this.roughness = options.roughness ?? 0.5; this.metalness = options.metalness ?? 0; }
+}
